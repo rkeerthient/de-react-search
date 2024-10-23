6c94530e7198bd4c70b47792840c8411128bbdde
rkeerthient <rkeerthi@yext.com>
2024-10-24 00:13:41 +0530

    made sort options responsive

--- src/components/SortDropdown.tsx
+++ src/components/SortDropdown.tsx
@@ -50,9 +50,9 @@ const SortDropdown = ({ sortOptions }: SortDropdownProps) => {
   return (
     <div className="relative text-sm ml-auto">
       <Menu as="div" className="relative inline-block text-left">
-        <MenuButton className="inline-flex w-56 justify-between gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        <MenuButton className="inline-flex w-full md:w-56 justify-between gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           <div className="flex flex-col justify-start items-baseline">
-            <span className="text-sm font-medium">Sort Options</span>
+            <span className="text-xs md:text-sm font-medium">Sort Options</span>
             <span className="text-xs  mt-1">
               {selectedItem || "Select an option"}
             </span>
@@ -65,7 +65,7 @@ const SortDropdown = ({ sortOptions }: SortDropdownProps) => {
 
         <MenuItems
           transition
-          className="absolute right-0 z-10 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition-all ease-out duration-200"
+          className="absolute right-0 z-10 w-full md:w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition-all ease-out duration-200"
         >
           <div className="py-1">
             {sortOptions
@@ -77,7 +77,7 @@ const SortDropdown = ({ sortOptions }: SortDropdownProps) => {
                 >
                   {({ active }) => (
                     <div
-                      className={`flex h-10 w-56 items-center px-2 ${
+                      className={`md:text-base text-xs flex h-6 md:h-10  w-full md:w-56 items-center px-2 ${
                         active ? "bg-gray-200" : "bg-white"
                       } hover:bg-gray-300 cursor-pointer`}
                     >
