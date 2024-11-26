dace3795e7e8c78f49fb756a5b70ee21dfb0811d
rkeerthient <rkeerthi@yext.com>
2024-11-26 17:41:31 +0530

    fixed video card

--- src/components/cards/ProductProminentVideo.tsx
+++ src/components/cards/ProductProminentVideo.tsx
@@ -11,22 +11,24 @@ const ProductProminentVideo = ({ result }: CardProps<any>) => {
     landingPageUrl,
     c_secondaryCTA,
     youtube_videoURL,
+    videos,
   } = result.rawData;
+
   return (
     <article id={`video-card-${id}`} className={`border rounded-lg `}>
       <header className={`relative flex flex-col `}>
-        <a
-          href={landingPageUrl}
+        <div
+          aria-label="video frame"
           className={`group aspect-square block overflow-hidden rounded-t-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 `}
         >
           <iframe
-            src={`https://www.youtube.com/embed/${id}`}
+            src={`https://www.youtube.com/embed/${videos[0].video.url.split("=")[1]}`}
             allow="autoplay; encrypted-media"
-            allowFullScreen
-            title="video"
+            allowFullScreen={true}
+            title="YouTube video"
             className="border-0 w-full h-full"
           />
-        </a>
+        </div>
         <h2 className="text-lg font-bold px-2 mt-4">
           <a href={youtube_videoURL}>{name}</a>
         </h2>
